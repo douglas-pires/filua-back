1713e168d0e83d4026ef92902d35bd1251f12d1c
Douglas Pires <douglas.pires@live.com>
2020-01-25 11:54:28 -0300

    create eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true
+  },
+  parserOptions: {
+    parser: '@typescript-eslint/parser'
+  },
+  extends: [
+    '@nuxtjs',
+    'prettier',
+    'prettier/vue',
+    'plugin:prettier/recommended',
+    'plugin:nuxt/recommended',
+    'plugin:vue/strongly-recommended'
+  ],
+  plugins: ['prettier'],
+  // add your custom rules here
+  rules: {}
+}

0dac776f4b1e212969ae8815c41a31da0a4187f1
Douglas Pires <douglas.pires@live.com>
2020-02-02 01:21:53 -0300

    remove spacing

--- src/routes/products.ts
+++ src/routes/products.ts
@@ -5,11 +5,8 @@ import Product from '../db/schema/product';
 
 router.post('/image-upload', async (req, res) => {
   const products = req.body
-
   const dataSource = new ProductDataSource(Product)
-
   const result = await dataSource.addProducts(products)
-
   res.send(result)
 });
 

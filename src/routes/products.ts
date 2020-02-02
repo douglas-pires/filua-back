793af0b9f4fb9317de466534f79085a2e29904fa
Douglas Pires <douglas.pires@live.com>
2020-02-02 01:21:31 -0300

    insert many function

---
+++ src/routes/products.ts
@@ -0,0 +1,16 @@
+import express from 'express';
+const router = express.Router();
+import ProductDataSource from '../data-sources/product';
+import Product from '../db/schema/product';
+
+router.post('/image-upload', async (req, res) => {
+  const products = req.body
+
+  const dataSource = new ProductDataSource(Product)
+
+  const result = await dataSource.addProducts(products)
+
+  res.send(result)
+});
+
+export default router;

6217c71773917e4b4c63030d5f8c7c57672dec4d
Douglas Pires <douglas.pires@live.com>
2020-02-02 02:17:13 -0300

    update

--- src/routes/products.ts
+++ src/routes/products.ts
@@ -3,11 +3,15 @@ const router = express.Router();
 import ProductDataSource from '../data-sources/product';
 import Product from '../db/schema/product';
 
-router.post('/image-upload', async (req, res) => {
+router.post('/products', async (req, res) => {
   const products = req.body
-  const dataSource = new ProductDataSource(Product)
-  const result = await dataSource.addProducts(products)
-  res.send(result)
+  try {
+    const dataSource = new ProductDataSource(Product)
+    const result = await dataSource.addProducts(products)
+    res.send(result)
+  } catch (error) {
+    res.send(error)
+  }
 });
 
 export default router;
